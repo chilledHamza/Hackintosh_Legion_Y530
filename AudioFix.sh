6c73da45f65ff588caff23d257da95ff884175b6
chilledHamza <hamza.farooq@outlook.com>
2021-01-16 19:44:23 +0500

    Fixed ALCPlugFix permissions, Earlier variable naming was inconsistant. Script should work properly now.

--- AudioFix.sh
+++ AudioFix.sh
@@ -11,15 +11,14 @@ echo "
 DAEMON_PATH=/Library/LaunchDaemons/
 BIN_PATH=/usr/local/bin/
 TMP_PATH=/tmp/
-ALC_DAEMON=good.win.ALCPlugFix
 ALC_DAEMON_CONFIG=good.win.ALCPlugFix.plist
-ALCPlugFIX_BINARY=ALCPlugFix
-ALC_VERB_BINARY=alc-verb
+ALCPlugFix=ALCPlugFix
+ALC_VERB=alc-verb
 URL=https://raw.githubusercontent.com/chilledHamza/Hackintosh_Legion_Y7000_2018/main/ALCPlugFix/
 
 echo "Downloading required file"
-sudo curl -o $TMP_PATH$ALCPlugFIX_BINARY "$URL$ALCPlugFIX_BINARY"
-sudo curl -o $TMP_PATH$ALC_VERB_BINARY "$URL$ALC_VERB_BINARY"
+sudo curl -o $TMP_PATH$ALCPlugFix "$URL$ALCPlugFix"
+sudo curl -o $TMP_PATH$ALC_VERB "$URL$ALC_VERB"
 sudo curl -o $TMP_PATH$ALC_DAEMON_CONFIG "$URL$ALC_DAEMON_CONFIG"
 
 if [ ! -d "$BIN_PATH" ] ; then
@@ -27,23 +26,23 @@ if [ ! -d "$BIN_PATH" ] ; then
 fi
 
 echo "Copy file to destination place..."
-sudo cp $TMP_PATH$ALC_VERB_BINARY $BIN_PATH
-sudo cp $TMP_PATH$ALCPlugFIX_BINARY $BIN_PATH
+sudo cp $TMP_PATH$ALC_VERB $BIN_PATH
+sudo cp $TMP_PATH$ALCPlugFix $BIN_PATH
 sudo cp $TMP_PATH$ALC_DAEMON_CONFIG $DAEMON_PATH
-sudo rm $TMP_PATH$ALC_VERB_BINARY
-sudo rm $TMP_PATH$ALCPlugFIX_BINARY
+sudo rm $TMP_PATH$ALC_VERB
+sudo rm $TMP_PATH$ALCPlugFix
 sudo rm $TMP_PATH$ALC_DAEMON_CONFIG
 
 echo "Chmod ALCPlugFix..."
-sudo chmod 755 $BIN_PATH$ALC_VERB_BINARY
-sudo chown $USER:admin $BIN_PATH$ALC_VERB_BINARY
-sudo chmod 755 $BIN_PATH$ALC_FIX_BINARY
-sudo chown $USER:admin $BIN_PATH$ALC_FIX_BINARY
+sudo chmod 755 $BIN_PATH$ALC_VERB
+sudo chown $USER:admin $BIN_PATH$ALC_VERB
+sudo chmod 755 $BIN_PATH$ALCPlugFix
+sudo chown $USER:admin $BIN_PATH$ALCPlugFix
 sudo chmod 644 $DAEMON_PATH$ALC_DAEMON_CONFIG
 sudo chown root:wheel $DAEMON_PATH$ALC_DAEMON_CONFIG
 
 echo "Load ALCPlugFix..."
-if sudo launchctl list | grep --quiet $ALC_DAEMON; then
+if sudo launchctl list | grep --quiet good.win.ALCPlugFix; then
     echo "Stopping existing ALCPlugFix daemon."
     sudo launchctl unload $DAEMON_PATH$ALC_DAEMON_CONFIG
 fi
